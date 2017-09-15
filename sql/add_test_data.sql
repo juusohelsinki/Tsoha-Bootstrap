07b16af5080d0da3f7191d39569d4fe3e239f858
juusohelsinki <juuso.saranki@helsinki.fi>
2017-09-15 17:58:21 +0300

    Lisätty tietokantakomennot
    Lisätty tietokantakomennot

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,6 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO users (usersid, username, email, password, usertype, name, description) VALUES (0, 'john@usa.com', 'john@usa.com', 'Kalle123', '0', 'John Doe', 'John on länkkäri.');
+INSERT INTO bands (bandsid, bandname, description, bandimagepath) VALUES (0, 'John Doe orchestra', 'Rujoa bläkkistä', 'http://www.kgkgkkg.fi/kuvatus.jpg');
+INSERT INTO usersinbands (usersID, bandsID) VALUES (3, 4);
+INSERT INTO permissions (usersID, bandsID, userlevel) VALUES (3, 4, 0);
+INSERT INTO reviews (usersID, bandsID, review, stars) VALUES (3, 4, 'Ei nyt oikein lähde.', 1);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,44 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE users (
+    usersID SERIAL PRIMARY KEY,
+    username varchar(200) ,
+    email varchar(200),
+    password varchar(200),
+    usertype int,
+    name varchar(200),
+    description varchar(200)
+);
+
+CREATE TABLE bands (
+    bandsID SERIAL PRIMARY KEY,
+    bandName varchar(200),
+    description varchar(4000),
+    bandImagePath varchar(500)
+);
+
+CREATE TABLE permissions (
+    permissionsID SERIAL PRIMARY KEY,
+    usersID int,
+    bandsID int,
+    userLevel int,
+    FOREIGN KEY (usersID) REFERENCES users(usersID),
+    FOREIGN KEY (bandsID) REFERENCES bands(bandsID)
+);
+
+CREATE TABLE reviews (
+    reviewsID SERIAL PRIMARY KEY,
+    usersID int,
+    bandsID int,
+    review varchar(5000),
+    stars int,
+    FOREIGN KEY (usersID) REFERENCES users(usersID),
+    FOREIGN KEY (bandsID) REFERENCES bands(bandsID)
+);
+
+CREATE TABLE usersinbands (
+    usersinbandsID SERIAL PRIMARY KEY,
+    usersID int,
+    bandsID int,
+    FOREIGN KEY (usersID) REFERENCES users(usersID),
+    FOREIGN KEY (bandsID) REFERENCES bands(bandsID)
+);
